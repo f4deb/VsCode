7f498d3f6c3e5716b100a14e8827900ac1d393d4
Jerome <f4deb1@gmail.com>
2026-06-15 23:14:30 +0200

    MAJ I2C

--- CMOS74/components/i2c/i2cTools.c
+++ CMOS74/components/i2c/i2cTools.c
@@ -10,15 +10,50 @@
 #include "../charUtils/include/charUtils.h"
 #include "../uartUtils/include/uartUtils.h"
 
+#include "../cmos/include/i2cEeprom.h"
+#include "../cmos/include/saa1064.h"
+
+
 #define I2C_TOOL_TIMEOUT_VALUE_MS (50)
 #define ESP_I2C_PORT I2C_NUM_0
 #define ESP_I2C_SDA  GPIO_NUM_21
 #define ESP_I2C_SCL  GPIO_NUM_22
+#define LENGTH 48
+#define MASTER_FREQUENCY_400 400000
 
 static uint32_t i2c_frequency = 100 * 1000;
 static const char *TAG = "i2ctools";
 static i2c_master_bus_handle_t tool_bus_handle; //static i2c_master_bus_handle_t i2c_handle = NULL;
 static i2c_master_bus_config_t i2c_bus_config;
+
+static i2c_eeprom_handle_t eeprom_handle;
+
+static i2c_master_bus_handle_t bus_handle;
+
+
+
+
+static void disp_buf(uint8_t *buf, int len)
+{
+    int i;
+    for (i = 0; i < len; i++) {
+        printf("%02x ", buf[i]);
+        if ((i + 1) % 16 == 0) {
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -43,6 +78,25 @@ uint8_t I2cBusInit(void){
     };
 
     ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_config, &tool_bus_handle));
+
+
+        i2c_eeprom_config_t eeprom_config = {
+        .eeprom_device.scl_speed_hz = MASTER_FREQUENCY_400,
+        .eeprom_device.device_address = 0x50,
+        .addr_wordlen = 2,
+        .write_time_ms = 10,
+    };
+
+
+    uint32_t block_addr = 0x0010;
+    uint8_t buf[LENGTH];
+    for (int i = 0; i < LENGTH; i++) {
+        buf[i] = i;
+    }
+    uint8_t read_buf[LENGTH];
+    //ESP_ERROR_CHECK(i2c_eeprom_init(bus_handle, &eeprom_config, &eeprom_handle));
+
+
     return 0;
 }
 
